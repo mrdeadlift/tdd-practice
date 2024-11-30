ab66fb7e10f343f59885d4feab55d4426b7208db
mrdeadlift <mrdeadlift96@gmail.com>
2024-11-30 23:09:04 +0900

    2章完了時点

---
+++ demo/src/main/java/com/example/tdd/demo/money/Dollar.java
@@ -0,0 +1,12 @@
+package com.example.tdd.demo.money;
+
+public class Dollar {
+    int amount;
+    Dollar(int amount){
+        this.amount = amount;
+    }
+    
+    Dollar times(int multiplier){
+        return new Dollar(amount * multiplier);
+    }
+}
